cbcd9e3a18610203e9c25c5ff535d6e3f5f89006
boomauakim <boomauakim@gmail.com>
2021-11-02 23:52:14 +0700

    Update firestore client

--- infrastructure/firestore/firestore.go
+++ infrastructure/firestore/firestore.go
@@ -2,28 +2,37 @@ package firestore
 
 import (
 	"context"
+	"errors"
 	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
 )
 
-func FirestoreInit() (*firestore.Client, error) {
+func FirestoreInit() *firestore.Client {
 	ctx := context.Background()
-	serviceAccount := option.WithCredentialsFile("../../keys/serviceAccountKey.json")
+
+	_, p, _, _ := runtime.Caller(0)
+	rootDir := filepath.Join(filepath.Dir(p), "../..")
+	serviceAccountPath := rootDir + "/keys/serviceAccount.json"
+	if _, err := os.Stat(serviceAccountPath); errors.Is(err, os.ErrNotExist) {
+		log.Fatalf("file %s not exists.", serviceAccountPath)
+	}
+	serviceAccount := option.WithCredentialsFile(serviceAccountPath)
 
 	app, err := firebase.NewApp(ctx, nil, serviceAccount)
 	if err != nil {
 		log.Fatalln(err)
-		return nil, err
 	}
 
 	client, err := app.Firestore(ctx)
 	if err != nil {
 		log.Fatalln(err)
-		return nil, err
 	}
 
-	return client, nil
+	return client
 }
